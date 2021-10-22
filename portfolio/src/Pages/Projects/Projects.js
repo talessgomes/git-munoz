db240d4d931167d2da463fc0a92d4a98f3191e64
Tales gomes <tales.gomes_sl@hotmail.com>
2021-10-21 22:06:29 -0300

    coloquei-footer-criei-projetos-estilizei-um-pouco-o-projeto

--- portfolio/src/Pages/Projects/Projects.js
+++ portfolio/src/Pages/Projects/Projects.js
@@ -1,24 +1,26 @@
 import React from "react";
 import {ProjectsContainer,
-  ListProject
+  ListProject01,
+  ButtonProject
 } from "./index"
+import project01 from "../../img/imac.png"
 
 export const Projects = () => {
   return (
     <ProjectsContainer>
       <h2>Meus projetos</h2>
-      <ListProject>
-        <h2>Pokédex</h2>
+      <ListProject01>
+        <img src={project01}/>
         <div>
           <h2>Pokédex</h2>
           <p>Esse projeto foi feito com um grupo, nele é possível ver os detalhes do pokémon,
            da para adicionar os pokémons dentro da pokédex,
            também tem um filtro nele, onde é possível filtar o a busca.
             </p>
-            <a href="https://github.com/future4code/munoz-pokedex6" target="_blank"><button>Pokédex</button></a>
+            <ButtonProject><a href="https://github.com/future4code/munoz-pokedex6" target="_blank">Ver no Github</a></ButtonProject>
           </div>
 
-      </ListProject>
+      </ListProject01>
     </ProjectsContainer>
   );
 }
